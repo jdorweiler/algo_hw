02c3ff48f3a1d174f4856a9fa720e03db805cf23
JD <jddorweiler@gmail.com>
2014-05-15 10:28:19 -0400

    added plotting to prob 2

--- hw3/problem2.py
+++ hw3/problem2.py
@@ -1,34 +1,67 @@
 from pulp import *
 import numpy as np
-points = [[1,3], [2,5], [3,7], [5,11], [7,14], [8,15], [10,19]]
+import matplotlib as plt
+import pylab
 
 #set up variables
 x = LpVariable("X_i")
 y = LpVariable("Y_i")
 c = LpVariable("C")
 
-# Create the 'prob' variable to conmitain the problem data
-prob = LpProblem("min max line", LpMinimize)
 
-#objective to minimize
-# minimize the sum of all the distances from a line to each point
-for i in range(len(points)):
-	prob += points[i][0]*x+points[i][1]*y-c
+def main():
 
-prob += x > 8
-prob += y > 0
-# The problem data is written to an .lp file
-prob.writeLP("minmaxline.lp")
+	global x, y, c
 
-# The problem is solved using PuLP's choice of Solver
-prob.solve()
+	points = [[1,3], [2,5], [3,7], [5,11], [7,14], [8,15], [10,19]]
 
-# The status of the solution is printed to the screen
-print "Status:", LpStatus[prob.status]
+	# Create the 'prob' variable to conmitain the problem data
+	prob = LpProblem("min max line", LpMinimize)
 
-# Each of the variables is printed with it's resolved optimum value
-for v in prob.variables():
-    print v.name, "=", v.varValue
+	#objective to minimize
+	# minimize the sum of all the distances from a line to each point
+	for i in range(len(points)):
+		prob += lpSum(points[i][0]*x+points[i][1]*y-c)
 
-# The optimised objective function value is printed to the screen    
-print "Total net profit  = ", value(prob.objective)
+	prob += x >= 0.001
+	prob += y >= 0.001
+	# The problem data is written to an .lp file
+	prob.writeLP("minmaxline.lp")
+
+	# The problem is solved using PuLP's choice of Solver
+	prob.solve()
+
+	# The status of the solution is printed to the screen
+	print "Status:", LpStatus[prob.status]
+
+	# Each of the variables is printed with it's resolved optimum value
+	for v in prob.variables():
+	    print v.name, "=", v.varValue
+
+	# The optimised objective function value is printed to the screen    
+	print "Total net profit  = ", value(prob.objective)
+
+	plot(points, prob.variables())
+
+def plot(points, variables):
+
+	global x,y,c
+
+	x_points = []
+	y_points = []
+
+	# separate x,y for plotting
+	for i in range(len(points)):
+		x_points.append(points[i][0])
+		y_points.append(points[i][1])
+
+	t = np.arange(0.0, 2.0, 0.01)
+
+	line = x.varValue*t+c.varValue
+
+	plt.pyplot.scatter(x_points,y_points)
+	plt.pyplot.plot(line, 'r--');
+	plt.pyplot.show()
+
+if __name__ == '__main__':
+	main()
